962bd4d9669518cc4ba76c3ae0dbac6a909e9492
Michelle Ly <michellely@fb.com>
2022-06-13 16:24:33 -0700

    Added logout button and functionality

--- app/src/main/java/com/example/instaparse/MainActivity.java
+++ app/src/main/java/com/example/instaparse/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.instaparse;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.parse.ParseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnLogout = findViewById(R.id.btnLogout);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.logout, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.btnLogout) {
+            onLogoutButton();
+        }
+        return true;
+    }
+
+    void onLogoutButton() {
+        Toast.makeText(MainActivity.this, "Logged out!", Toast.LENGTH_LONG).show();
+        ParseUser.logOut();
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        Intent i = new Intent(this, LoginActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(i);
     }
 }
+

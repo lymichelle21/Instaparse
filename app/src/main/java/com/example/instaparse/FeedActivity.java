c4f0aeb3b7b80a4e16bf692c2dd6bd9d9a2e0765
Michelle Ly <michellely@fb.com>
2022-06-15 13:25:33 -0700

    Created activity feed and item post xml files

--- app/src/main/java/com/example/instaparse/FeedActivity.java
+++ app/src/main/java/com/example/instaparse/FeedActivity.java
@@ -1,7 +1,20 @@
 package com.example.instaparse;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class FeedActivity extends AppCompatActivity {
+
+    private RecyclerView rvPosts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+        rvPosts = findViewById(R.id.rvPosts);
+
+    }
 
 }

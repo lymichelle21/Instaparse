9493223890c59f167397d4e0b11c88ad9289e904
Michelle Ly <michellely@fb.com>
2022-06-15 15:56:35 -0700

    Post image, description, and user appear on detail screen

--- app/src/main/java/com/example/instaparse/PostDetailsActivity.java
+++ app/src/main/java/com/example/instaparse/PostDetailsActivity.java
@@ -10,6 +10,9 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.parse.ParseFile;
+
 import org.parceler.Parcels;
 
 import java.io.File;
@@ -21,6 +24,7 @@ public class PostDetailsActivity extends AppCompatActivity {
      TextView tvUsername;
      ImageView ivImage;
      TextView tvDescription;
+     TextView tvTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +34,18 @@ public class PostDetailsActivity extends AppCompatActivity {
         tvUsername = (TextView) findViewById(R.id.tvUsername);
         ivImage = (ImageView) findViewById(R.id.ivImage);
         tvDescription = (TextView) findViewById(R.id.tvDescription);
+        tvTime = (TextView) findViewById(R.id.tvTime);
+
+        post = (Post) Parcels.unwrap(getIntent().getParcelableExtra(Post.class.getSimpleName()));
+
+        tvDescription.setText(post.getDescription());
+        tvUsername.setText(post.getUser().getUsername());
+        tvTime.setText("INSERT TIME HERE");
+
+        ParseFile image = post.getImage();
+        if (image != null) {
+            Glide.with(getApplicationContext()).load(image.getUrl()).into(ivImage);
+        }
 
     }
 

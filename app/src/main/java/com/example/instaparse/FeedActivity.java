0bf3c45094c7b770d901ebe73df2467622a2e889
Michelle Ly <michellely@fb.com>
2022-06-15 13:50:31 -0700

    Added functionality to query posts and display them in the feed

--- app/src/main/java/com/example/instaparse/FeedActivity.java
+++ app/src/main/java/com/example/instaparse/FeedActivity.java
@@ -1,18 +1,26 @@
 package com.example.instaparse;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class FeedActivity extends AppCompatActivity {
 
-    private RecyclerView rvPosts;
+    private static int POST_LIMIT = 20;
     protected PostsAdapter adapter;
     protected List<Post> allPosts;
+    private RecyclerView rvPosts;
+    private String TAG = "FeedActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +29,33 @@ public class FeedActivity extends AppCompatActivity {
         rvPosts = findViewById(R.id.rvPosts);
         allPosts = new ArrayList<>();
         adapter = new PostsAdapter(this, allPosts);
+        rvPosts.setAdapter(adapter);
+        rvPosts.setLayoutManager(new LinearLayoutManager(this));
+        queryPosts();
+    }
+
+    private void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.setLimit(POST_LIMIT);
+        query.addDescendingOrder("createdAt");
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                // check for errors
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                for (Post post : posts) {
+                    Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
+                }
+                allPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 
 }
+
+

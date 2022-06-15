8d77b1abbef552b1b5ca1e3c8a271de43186cf41
Michelle Ly <michellely@fb.com>
2022-06-15 13:42:26 -0700

    Created PostAdapter for RecyclerView to hold posts

--- app/src/main/java/com/example/instaparse/FeedActivity.java
+++ app/src/main/java/com/example/instaparse/FeedActivity.java
@@ -5,16 +5,22 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FeedActivity extends AppCompatActivity {
 
     private RecyclerView rvPosts;
+    protected PostsAdapter adapter;
+    protected List<Post> allPosts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
         rvPosts = findViewById(R.id.rvPosts);
-
+        allPosts = new ArrayList<>();
+        adapter = new PostsAdapter(this, allPosts);
     }
 
 }
